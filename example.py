3b0c2d2a535b1fa97014a71b4485e8349a844ff3
yWo-o <95074199+yWo-o@users.noreply.github.com>
2021-11-28 15:55:12 +0800

    Update example.py

--- example.py
+++ example.py
@@ -2,10 +2,26 @@ otmoptionprice_grouped = otmoptionprice.groupby('Time')
 
 # pass no keyword arguments -- using default n = 100
 
+# initial calibration
+
 rslt = applyParallel(otmoptionprice_grouped,sviInit)
 
 otmoptionprice['Moneyness'] = np.log( otmoptionprice['Strike']/otmoptionprice['FuturesPrice'] )
 
 dfk = otmoptionprice[['Time','Maturity','Moneyness']].groupby(['Time','Maturity'])['Moneyness'].agg([np.amin,np.amax]).rename(columns={'amin':'k_min','amax':'k_max'})
 
+# dfsvipInit is initial calibration results parameter DataFrame
+
 dfsvipInit = rslt.join(dfk,how='inner')
+
+# recalibrate each slice
+
+rslt = applyParallel(otmoptionprice_grouped,sviSec,dfsvip=dfsvipInit)
+
+dfsvipSec = rslt.join(dfk,how='inner')
+
+d = 'yyyy-mm-dd'
+
+sviSurface(d,otmoptionprice,dfsvipInit)
+
+sviSurface(d,otmoptionprice,dfsvipSec)
