ecd7c2ad7e0cec1a68d8eca921b7e566e326f867
yWo-o <95074199+yWo-o@users.noreply.github.com>
2021-11-28 16:20:33 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,3 +1,23 @@
+import pandas as pd
+import numpy as np
+from scipy import interpolate
+from scipy.optimize import minimize
+from scipy.optimize import NonlinearConstraint
+from scipy.optimize import Bounds
+from joblib import Parallel, delayed
+import multiprocessing
+import sympy
+from sympy.solvers import solve
+from sympy import Symbol
+from matplotlib.pyplot import figure
+import matplotlib.pyplot as plt
+
+# otmoptionprice is a DataFrame which contains
+
+# ['Time'], ['Maturity'], ['FuturesPrice'], ['Strike'], ['ImpliedVolatility']
+
+# for out-of-the-money (OTM) options of a SINGLE underlying asset
+
 otmoptionprice_grouped = otmoptionprice.groupby('Time')
 
 # pass no keyword arguments -- using default n = 100
@@ -21,6 +41,8 @@ rslt = applyParallel(otmoptionprice_grouped,sviSec,dfsvip=dfsvipInit)
 dfsvipSec = rslt.join(dfk,how='inner')
 
 d = 'yyyy-mm-dd'
+
+# depict figures
 
 sviSurface(d,otmoptionprice,dfsvipInit)
 
